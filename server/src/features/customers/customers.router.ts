f8ade012392d356220bb0ccc6d4d1f84d7e235c0
Wesley Choate <wesley.choate@gmail.com>
2025-01-27 22:19:05 +0000

    working

--- server/src/features/customers/customers.router.ts
+++ server/src/features/customers/customers.router.ts
@@ -1,3 +1,18 @@
 import express from "express";
+import { getCustomerDetail, getCustomers } from "./customers.service";
 
 export const customersRouter = express.Router();
+
+customersRouter.get("/", async (req, res) => {
+  const customers = await getCustomers();
+  res.json(customers);
+});
+
+customersRouter.get("/:id", async (req, res) => {  
+  const customer = await getCustomerDetail(req.params.id);
+  if (customer != null) {    
+    res.json(customer);
+  } else {
+    res.status(404).json({message:"Customer Not Found"});
+  }
+})
--- server/src/features/items/items.router.ts
+++ server/src/features/items/items.router.ts
@@ -1,6 +1,27 @@
 import express from "express";
+import { resolve } from "path";
+import { getItemDetail, getItems } from "./items.service";
 
 export const itemsRouter = express.Router();
+
+itemsRouter.get("/", async (req, res) => {
+  const items = await getItems();
+  items.forEach((item) => {
+    item.imageUrl = buildImageUrl(req, item.id);
+  });
+  res.json(items);
+});
+
+itemsRouter.get("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const item = await getItemDetail(id);
+  if (item != null) {
+    item.imageUrl = buildImageUrl(req, item.id);
+    res.json(item);
+  } else {
+    res.status(404).json({message:"Item Not Found"});
+  }
+})
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 function buildImageUrl(req: any, id: number): string {
