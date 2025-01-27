c4e348033b0aa7c50c89b9394b68b3392f4ce329
Wesley Choate <wesley.choate@gmail.com>
2025-01-27 23:14:59 +0000

    more chgs

--- server/src/features/customers/customers.router.ts
+++ server/src/features/customers/customers.router.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { getCustomerDetail, getCustomers } from "./customers.service";
+import { getCustomerDetail, getCustomers, searchCustomers } from "./customers.service";
+import { getOrdersForCustomer } from "../orders/orders.service";
 
 export const customersRouter = express.Router();
 
@@ -10,6 +11,20 @@ customersRouter.get("/", async (req, res) => {
 
 customersRouter.get("/:id", async (req, res) => {  
   const customer = await getCustomerDetail(req.params.id);
+  if (customer != null) {    
+    res.json(customer);
+  } else {
+    res.status(404).json({message:"Customer Not Found"});
+  }
+})
+
+customersRouter.get("/:id/orders", async (req, res) => {  
+  const orders = await getOrdersForCustomer(req.params.id);
+  res.json(orders);
+})
+
+customersRouter.get("/search/:query", async (req, res) => {  
+  const customer = await searchCustomers(req.params.query);
   if (customer != null) {    
     res.json(customer);
   } else {
--- server/src/features/orders/orders.router.ts
+++ server/src/features/orders/orders.router.ts
@@ -1,3 +1,29 @@
 import express from "express";
+import { getOrders } from "./orders.service";
+import { skip } from "node:test";
 
 export const ordersRouter = express.Router();
+
+ordersRouter.get("/", async (req, res) => {
+    const query = req.query;
+    const take = query.take;
+    const skip = query.skip;
+
+    if (
+        take &&
+        typeof take === "string" &&
+        parseInt(take) > 0 &&
+        skip &&
+        typeof skip === "string" &&
+        parseInt(skip) > -1
+     ) {
+        const orders = await getOrders(parseInt(skip), parseInt(take));
+        res.json(orders);
+    } else {
+        res.status(400).json({
+            message: 
+            "Take and skip query parameters are required. " +
+            "Take must be greater than 0 and skip must be great than -1"
+        });
+    }
+})
